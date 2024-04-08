ac764c329eb69bedf86f240dd9798a118a055c16
demitryedwards <dxyz773@gmail.com>
2024-04-08 15:32:11 -0400

    Add ErrorFallBack

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./pages/ErrorFallback.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ErrorBoundary
+    FallbackComponent={<ErrorFallback />}
+    onReset={() => window.location.replace("/")}
+  >
+    <App />
+  </ErrorBoundary>,
+);
