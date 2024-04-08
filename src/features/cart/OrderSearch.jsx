1de741d5cd960146d245f32d32c47f868c7ab313
demitryedwards <dxyz773@gmail.com>
2024-04-08 19:32:48 -0400

    Mobile style changes

--- src/features/cart/OrderSearch.jsx
+++ src/features/cart/OrderSearch.jsx
@@ -6,7 +6,7 @@ function OrderSearch() {
   const navigate = useNavigate();
 
   return (
-    <div className="ml-auto mr-auto flex min-h-dvh flex-col gap-5 pb-10 pl-3 pr-4 pt-14 font-serif tracking-wider md:w-11/12 md:px-5 lg:px-16">
+    <div className="flex min-h-dvh w-[20rem] flex-col gap-5 pb-10 pl-4 pr-4 pt-14 font-serif tracking-wider sm:ml-auto sm:mr-auto md:w-11/12 md:px-5 lg:px-16">
       <div className="ml-auto mr-auto mt-20 font-serif tracking-wide">
         <h3 className="border-b-2 pb-1 text-xl">Order Search</h3>
         <div className="mt-12">
